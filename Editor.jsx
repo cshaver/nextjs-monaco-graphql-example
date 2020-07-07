103ef5fb5a7ebb2e124c05216d633102eb8d18a8
Cristina Shaver <cristina.shaver@gmail.com>
2020-07-06 22:05:13 -0700

    use monaco-graphql api

--- Editor.jsx
+++ Editor.jsx
@@ -2,13 +2,25 @@ import MonacoEditor from 'react-monaco-editor';
 
 import 'monaco-graphql/esm/monaco.contribution';
 
+import { api as GraphQLAPI } from 'monaco-graphql';
+
 import sample from './code-sample'
+
+const SCHEMA_URL = 'https://api.spacex.land/graphql/';
 
 window.MonacoEnvironment.getWorkerUrl = (moduleId, label) => {
   if (label === 'json') return '/_next/static/json.worker.js'
   if (label === 'graphqlDev') return '/_next/static/monaco-graphql.worker.js'
   return '/_next/static/editor.worker.js'
 }
+
+GraphQLAPI.setFormattingOptions({
+  prettierConfig: {
+    printWidth: 120,
+  },
+});
+
+GraphQLAPI.setSchemaConfig({ uri: SCHEMA_URL });
 
 export default function Editor() {
   return (
